246c133f26d9824da6b92b290d85e87554aaeb18
dukjjulee <nancy0523@naver.com>
2025-08-14 13:29:06 +0900

    feat: Add UserService, UserController

---
+++ src/main/java/org/example/schedulenote/User/controller/UserController.java
@@ -0,0 +1,53 @@
+package org.example.schedulenote.User.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.schedulenote.User.dto.UserResponse;
+import org.example.schedulenote.User.dto.UserSaveRequest;
+import org.example.schedulenote.User.dto.UserSaveResponse;
+import org.example.schedulenote.User.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<UserSaveResponse> saveUsers(
+            @RequestBody UserSaveRequest request
+    ) {
+        return ResponseEntity.ok(userService.save(request));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getUsers() {
+        List<UserResponse> users = userService.findUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> getUsers(
+            @PathVariable long scheduleId,
+            long userId) {
+        return ResponseEntity.ok(userService.findUser(userId));
+    }
+
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> UpdateUser(
+            @PathVariable long userId,
+            @RequestBody UserSaveRequest request
+    ) {
+        return  ResponseEntity.ok(userService.updateUser(userId, request));
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void deletUser(
+            @PathVariable long userId
+    ) {
+        userService.deleteUser(userId);
+    }
+}
---
+++ src/main/java/org/example/schedulenote/User/service/UserService.java
@@ -0,0 +1,94 @@
+package org.example.schedulenote.User.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.schedulenote.User.dto.UserResponse;
+import org.example.schedulenote.User.dto.UserSaveRequest;
+import org.example.schedulenote.User.dto.UserSaveResponse;
+import org.example.schedulenote.User.entity.User;
+import org.example.schedulenote.User.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    //유저 생성
+    @Transactional
+    public UserSaveResponse save(UserSaveRequest request) {
+        User user = new User(
+                request.getAuthor(),
+                request.getEmail()
+        );
+
+        User savedUser = userRepository.save(user);
+        return new UserSaveResponse(
+                savedUser.getId(),
+                savedUser.getAuthor(),
+                savedUser.getEmail(),
+                savedUser.getCreatedAt(),
+                savedUser.getModifiedAt()
+        );
+    }
+
+    //유저 목록 조회
+    @Transactional(readOnly = true)
+    public List<UserResponse> findUsers() {
+        List<User> users = userRepository.findAll();
+        List<UserResponse> dtos = new ArrayList<>();
+
+        for(User user : users) {
+            UserResponse userResponse = new UserResponse (
+                    user.getId(),
+                    user.getAuthor(),
+                    user.getEmail(),
+                    user.getCreatedAt(),
+                    user.getModifiedAt()
+            );
+            dtos.add(userResponse);
+        }
+        return dtos;
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse findUser(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () ->new IllegalArgumentException("User를 찾을 수 없습니다.")
+        );
+        return new UserResponse(
+                user.getId(),
+                user.getAuthor(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
+        );
+    }
+
+    //유저 수정
+    @Transactional
+    public UserResponse updateUser(long userId, UserSaveRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("User을 찾을 수 없습니다.")
+        );
+        user.UpdateUserInformation(request.getAuthor(), request.getEmail());
+        return new UserResponse(
+                user.getId(),
+                user.getAuthor(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
+        );
+    }
+
+    //유저 삭제
+    @Transactional
+    public void deleteUser(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("User를 찾을 수 없습니다.")
+        );
+    }
+}
